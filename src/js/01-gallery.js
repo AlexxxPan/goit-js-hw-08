7082bb250bf3964be1bc6ec63db757c4687c314d
AlexxxPan <alxtouch@gmail.com>
2023-01-22 01:30:38 +0200

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+
+const result = createGallary(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", result);
+
+
+function createGallary(galleryItems) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>`,
+    ""
+  );
+  }
+
+new SimpleLightbox('.gallery a', {captionDelay:250, captionsData:"alt"})
