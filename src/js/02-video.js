dd2afdedfb0eeb6f3b050bd76b4896eb1d6d8d53
AlexxxPan <alxtouch@gmail.com>
2023-01-22 09:51:57 +0200

    add catch error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(function (t) {
+player.on(
+  'timeupdate',
+  throttle(t => {
     localStorage.setItem('videoplayer-current-time', t.seconds);
-    }, 1000)
+  }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-    
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(error => {
+    console.error(error);
+  });
