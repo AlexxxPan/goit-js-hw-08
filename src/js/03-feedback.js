1bd2dc3197b1ecfd4334bf2013263814ff4ddec3
Alex Pan <alxtouch@gmail.com>
2023-01-23 23:49:40 +0200

    Add new task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+import throttle from 'lodash.throttle';
+const feedbackform = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
 
+feedbackform.addEventListener('input', throttle (e => {
+
+}));
